c18c874f445641d887d839ee1d59dda5e42aa270
Derek Root <dkwroot@users.noreply.github.com>
2019-11-13 02:40:06 -0600

    Updates

---
+++ precache-manifest.8d8dd6feff8ef176fc355049e8d19d52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61815ccb67f906e72614bdefcf32b095",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e067fde379adf62fd48e",
+    "url": "/portfolio/static/css/2.5c5f4c00.chunk.css"
+  },
+  {
+    "revision": "9bb5407c9f9ecd2938b6",
+    "url": "/portfolio/static/css/main.586f013a.chunk.css"
+  },
+  {
+    "revision": "e067fde379adf62fd48e",
+    "url": "/portfolio/static/js/2.8b36b060.chunk.js"
+  },
+  {
+    "revision": "9bb5407c9f9ecd2938b6",
+    "url": "/portfolio/static/js/main.375c030b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "914b625fb4b07cd39f25be4beda97834",
+    "url": "/portfolio/static/media/graphics_product.914b625f.png"
+  },
+  {
+    "revision": "b9ea0a901e44e1dcb9b1f429db25ef46",
+    "url": "/portfolio/static/media/paintApp.b9ea0a90.png"
+  },
+  {
+    "revision": "9ee4db3745ac00bcb1e8bd579c6650d5",
+    "url": "/portfolio/static/media/tool_world.9ee4db37.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ff8df9909e21db3290ef0be9426c24d.js"
+  "/portfolio/precache-manifest.8d8dd6feff8ef176fc355049e8d19d52.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
